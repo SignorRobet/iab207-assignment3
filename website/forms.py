80f7c8d55637d0ad469082d5ab072b0c97280cab
Simon <simon.diflorio@gmail.com>
2022-10-09 21:52:53 +1000

    login/rego, db model inits
    Added login and registration functionality
    removed unecessary init functions from models.py classes (see constructor link)

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,9 @@
 
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
+from wtforms.fields import (
+    TextAreaField, SubmitField, StringField, PasswordField,
+    DateField
+)
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 
 
@@ -10,16 +13,22 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[InputRequired('Enter user password')])
     submit = SubmitField("Login")
 
- # this is the registration form
-
 
 class RegisterForm(FlaskForm):
+    '''
+    This is the registration form
+    '''
     user_name = StringField("User Name", validators=[InputRequired()])
-    email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
+    email = StringField("Email Address", validators=[Email("Please enter a valid email")])
     # linking two fields - password should be equal to data entered in confirm
     password = PasswordField("Password", validators=[InputRequired(),
                                                      EqualTo('confirm', message="Passwords should match")])
     confirm = PasswordField("Confirm Password")
+
+    first_name = StringField("First Name", validators=[InputRequired()])
+    last_name = StringField("Last Name", validators=[InputRequired()])
+    phone = StringField("Phone", validators=[InputRequired()])
+    dob = DateField("Date of Birth", validators=[InputRequired()])
 
     # submit button
     submit = SubmitField("Register")
