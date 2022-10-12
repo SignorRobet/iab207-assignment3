87cf86f70a69916eb2c48e6e86fce89909abc5ce
Simon <simon.diflorio@gmail.com>
2022-10-12 12:17:50 +1000

    db model, login page, register validation

--- website/forms.py
+++ website/forms.py
@@ -4,7 +4,7 @@ from wtforms.fields import (
     TextAreaField, SubmitField, StringField, PasswordField,
     DateField
 )
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.validators import InputRequired, Length, Email, EqualTo, Regexp
 
 
 # creates the login information
@@ -18,11 +18,23 @@ class RegisterForm(FlaskForm):
     '''
     This is the registration form
     '''
-    user_name = StringField("User Name", validators=[InputRequired()])
+    user_name = StringField(
+        "User Name",
+        validators=[InputRequired(),
+                    Regexp(
+                        '([A-Za-z][A-Za-z0-9\-_]*[A-Za-z0-9]){1\}',
+                        message=('User name can only contain letters, numbers, hypens and underscores.\n' +
+                                 'Must start with a letter and end with a letter or number.')),
+                    Length(
+                        min=3, max=20, message='User name must be between 3 and 20 characters long')])
+
     email = StringField("Email Address", validators=[Email("Please enter a valid email")])
     # linking two fields - password should be equal to data entered in confirm
-    password = PasswordField("Password", validators=[InputRequired(),
-                                                     EqualTo('confirm', message="Passwords should match")])
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(),
+                    Length(min=4, message="Password must be at least 4 characters long"),
+                    EqualTo('confirm', message="Passwords should match")])
     confirm = PasswordField("Confirm Password")
 
     first_name = StringField("First Name", validators=[InputRequired()])
