33b987cd383abd36eb845c998ae1201f72c22c68
applesnsoranges <99218433+applesnsoranges@users.noreply.github.com>
2022-10-12 11:59:16 +1000

    base/index/search html
    More html combining, base index, and create search results page
    
    Put a route in for search results and rendered the template

--- website/views.py
+++ website/views.py
@@ -6,3 +6,8 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/searchresults')
+def searchresults():
+    return render_template('searchresults.html')
+
