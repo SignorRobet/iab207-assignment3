1b7108466e0a3f5dc5f64f4156adee07bc60c145
aiuaierhuigj <iansearlecrossley@gmail.com>
2022-11-02 17:47:16 +1000

    my Concerts
    Linked DB to form and html
    -not fully working

--- website/views.py
+++ website/views.py
@@ -27,6 +27,8 @@ def searchresults():
 @bp.route('/myconcerts')
 @login_required
 def myconcerts():
+    disp_bookings = Booking.query.all()
+    disp_events = Event.query.all()
     return render_template('myconcerts.html', title='My Concerts')
 
 @bp.route('/createevent', methods = ['GET', 'POST'])
